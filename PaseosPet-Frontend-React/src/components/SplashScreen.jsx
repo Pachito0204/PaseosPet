f2fa08bcba894a354094e7cc9faa5582a39aa476
Pachito0204 <jorgearosasrmx@gmail.com>
2025-10-26 16:19:22 -0600

    Update SplashScreen.jsx

--- PaseosPet-Frontend-React/src/components/SplashScreen.jsx
+++ PaseosPet-Frontend-React/src/components/SplashScreen.jsx
@@ -1,20 +1,18 @@
 import React, { useEffect } from "react";
 import "./SplashScreen.css";
-import logo from "../assets/paseospet-logo.png"; // coloca aquí tu logo
+import logo from "../assets/paseospet-logo.png"; // tu logo
 
 const SplashScreen = ({ onFinish }) => {
   useEffect(() => {
-    const timer = setTimeout(onFinish, 2500); // 2.5 segundos de duración
+    const timer = setTimeout(onFinish, 4000); // duración aumentada a 4 segundos
     return () => clearTimeout(timer);
   }, [onFinish]);
 
   return (
     <div className="splash-container">
       <img src={logo} alt="PaseosPet" className="splash-logo" />
-      <h2 className="splash-text">PaseosPet</h2>
     </div>
   );
 };
 
 export default SplashScreen;
-
