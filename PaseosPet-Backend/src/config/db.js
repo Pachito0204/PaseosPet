e525d11ac2e1a31d0fac9046795af2c3b23e2131
Pachito0204 <jorgearosasrmx@gmail.com>
2025-10-26 20:31:17 -0600

    Refactor db.js to use CommonJS syntax

--- PaseosPet-Backend/src/config/db.js
+++ PaseosPet-Backend/src/config/db.js
@@ -1,13 +1,13 @@
-import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
-dotenv.config();
+const { Sequelize } = require('sequelize');
 
-export const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres",
+const sequelize = new Sequelize(process.env.DATABASE_URL || 'postgresql://paseospet_db_user:NIiqVSxr59BKVbMsyhub3bqeSA2nzCAM@dpg-d3v77cu3jp1c7389a14g-a.oregon-postgres.render.com/paseospet_db', {
+  dialect: 'postgres',
   dialectOptions: {
     ssl: {
-      require: true, // Render exige SSL
-      rejectUnauthorized: false, // Permite certificados autofirmados
-    },
-  },
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 });
+
+module.exports = { sequelize };
