52892f0a5a00044add77410a3a9115483c3c9fae
Pachito0204 <jorgearosasrmx@gmail.com>
2025-11-01 08:12:35 -0600

    Delete PaseosPet-Backend directory

--- PaseosPet-Backend/src/routes/pets.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import { getPets, addPet } from '../controllers/petController.js';
-const router = express.Router();
-router.get('/', getPets);
-router.post('/', addPet);
-export default router;
--- PaseosPet-Backend/src/routes/walks.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import { getAvailableWalks, createWalk } from '../controllers/walkController.js';
-const router = express.Router();
-router.get('/available', getAvailableWalks);
-router.post('/', createWalk);
-export default router;
